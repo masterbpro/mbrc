b12555dc86bfb7acf637821265d774e3254db7fe
masterbpro <iserver12345@gmail.com>
2024-03-23 21:56:56 +0300

    delete values from ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,7 @@ resource "hcloud_load_balancer_target" "main" {
 
 resource "hcloud_load_balancer_service" "main-http" {
   load_balancer_id = hcloud_load_balancer.main.id
+  proxyprotocol    = true
   protocol         = "tcp"
   listen_port      = 80
   destination_port = 80
@@ -27,6 +28,7 @@ resource "hcloud_load_balancer_service" "main-http" {
 
 resource "hcloud_load_balancer_service" "main-https" {
   load_balancer_id = hcloud_load_balancer.main.id
+  proxyprotocol    = true
   protocol         = "tcp"
   listen_port      = 443
   destination_port = 443
